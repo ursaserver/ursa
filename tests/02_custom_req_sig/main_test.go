6b4840bb52e49ae4600d776b28fc07c6ea798172
suman chapai <sumanchapai@gmail.com>
2023-10-20 18:46:24 -0500

    Run tests using ip address instead of hostname

--- tests/02_custom_req_sig/main_test.go
+++ tests/02_custom_req_sig/main_test.go
@@ -40,16 +40,16 @@ func helperChecker(tests []test, t *testing.T) {
 	}
 }
 
-// func TestUnAuthenticated(t *testing.T) {
-// 	tests := make([]test, 0)
-// 	// Send unauthenticated request
-// 	for i := 0; i < baseRate.Capacity; i++ {
-// 		tests = append(tests, test{expectCode: 200, url: upstreamUrl()})
-// 	}
-// 	// Send one more request, expect to be rate limited
-// 	tests = append(tests, test{expectCode: 429, url: upstreamUrl()})
-// 	helperChecker(tests, t)
-// }
+func TestUnAuthenticated(t *testing.T) {
+	tests := make([]test, 0)
+	// Send unauthenticated request
+	for i := 0; i < baseRate.Capacity; i++ {
+		tests = append(tests, test{expectCode: 200, url: upstreamUrl()})
+	}
+	// Send one more request, expect to be rate limited
+	tests = append(tests, test{expectCode: 429, url: upstreamUrl()})
+	helperChecker(tests, t)
+}
 
 func TestAuthenticated(t *testing.T) {
 	// Start rate limiter
@@ -83,5 +83,5 @@ func TestInvalidAuth(t *testing.T) {
 }
 
 func upstreamUrl() string {
-	return fmt.Sprintf("http://localhost:%d", ratelimiterPort)
+	return fmt.Sprintf("http://127.0.0.1:%d", ratelimiterPort)
 }
