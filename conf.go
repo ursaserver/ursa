d4e12a74a8e63dc973236ebf2e34bd847836e2d1
suman chapai <sumanchapai@gmail.com>
2023-10-23 09:41:20 -0500

    Cleanup

--- conf.go
+++ conf.go
@@ -8,17 +8,9 @@ import (
 type Conf struct {
 	Upstream *url.URL
 	Routes   []Route
-	// Todo add default rates
-	// DefaultRates map[RateBy]rate
-	// TODO add non options to specify non-rate limiting routes
-	// nonRateLimit []NonRateLimitRoute
 }
 
 type Route struct {
 	Pattern *regexp.Regexp // regex describing HTTP path to match
 	Rates   RouteRates
 }
-
-// type NonRateLimitRoute struct {
-// 	Pattern *regexp.Regexp // regex describing HTTP path to match
-// }
